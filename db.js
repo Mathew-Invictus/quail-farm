24b4ddeb381605319cfd0b7c0ba4bdb7a3dc9f5d
Mathew-Invictus <mhickey@invictustechnology.io>
2026-04-09 13:33:50 -0500

    Add files via upload

---
+++ db.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://sirdazuuyufjctmumoal.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_iYF9_1iTvZEcMrDcyZwzqA_K0lPLKdH';
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
